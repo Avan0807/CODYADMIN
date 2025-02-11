0d4b0f71dcf1514a423dbfcb41a66f3090189565
Avan0807 <van080703@gmail.com>
2025-02-11 14:48:29 +0700

    ok

--- routes/api.php
+++ routes/api.php
@@ -25,9 +25,7 @@ use App\Http\Controllers\Api\ApiAppointmentController;
 Route::post('admin/login', [ApiAuthAdminController::class, 'adminLogin']);
 Route::post('admin/logout', [ApiAuthAdminController::class, 'logout'])->middleware('auth:sanctum');
 
-// dm may
 
-// resset may cc 
 
 // =================== DOCTOR AUTHENTICATION ===================
 Route::prefix('doctor')->group(function () {
